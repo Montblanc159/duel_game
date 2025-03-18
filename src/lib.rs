c9b06d1eaef91ff34eb2e645e45e4a165bed79f8
GuillaumeZ <guillaume.zaugg@gmail.com>
2025-03-18 23:35:24 +0100

    add default font

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{asset::load_internal_binary_asset, prelude::*};
 
 mod camera;
 mod game;
@@ -8,6 +8,15 @@ pub struct AppPlugin;
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(DefaultPlugins);
+
+        // Change default font
+        load_internal_binary_asset!(
+            app,
+            TextFont::default().font,
+            "../assets/fonts/yoster.ttf",
+            |bytes: &[u8], _path: String| { Font::try_from_bytes(bytes.to_vec()).unwrap() }
+        );
+
         app.add_plugins(camera::plugin);
         app.add_plugins(game::plugin);
 
